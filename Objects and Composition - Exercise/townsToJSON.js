a13ab1f32445a5b14b048e3e454ef32720278df2
Svetoslav Stoyanov <sgs.vss.svs.mvs.vvs@gmail.com>
2022-06-02 23:54:30 +0300

    Update townsToJSON.js

--- Objects and Composition - Exercise/townsToJSON.js
+++ Objects and Composition - Exercise/townsToJSON.js
@@ -1,8 +1,22 @@
 function townsToJSON(arr) {
+    let towns = []
+    let headings = arr.shift().split('|').filter(el => el !== '').map(el => el.trim())
+
     for (const line of arr) {
-        let tokens = line.split('|').filter(el => el !== '').map(el=> el.trim())
-        console.log(tokens);
+        let tokens = line.split('|').filter(el => el !== '').map(el => el.trim())
+        for (let i = 0; i < tokens.length; i++) {
+            if (tokens[i] == Number(tokens[i])) {
+                tokens[i] = Number(Number(tokens[i]).toFixed(2))
+
+            }
+        }
+        let obj = {}
+        for (let i = 0; i < tokens.length; i++) {
+            obj[headings[i]] = tokens[i]
+        }
+        towns.push(obj)
     }
+    console.log(JSON.stringify(towns));
 }
 townsToJSON(['| Town | Latitude | Longitude |',
     '| Sofia | 42.696552 | 23.32601 |',
