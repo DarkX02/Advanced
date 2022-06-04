b1f35567a066c39c307d9cd56e2f9eeb4523293f
Svetoslav Stoyanov <sgs.vss.svs.mvs.vvs@gmail.com>
2022-06-04 14:29:14 +0300

    Update SumTable.js

--- DOM Introduction -  Lab/04. JS-Advanced-DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
+++ DOM Introduction -  Lab/04. JS-Advanced-DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
@@ -1,3 +1,9 @@
 function sumTable() {
-
+    let prices = Array.from(document.querySelectorAll('td:nth-child(2n)'))
+    let result = prices.pop()
+    let sum = 0
+    for (const el of prices) {
+        sum += Number(el.textContent)
+    }
+    result.textContent = sum
 }
