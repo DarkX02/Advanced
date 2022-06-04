0f340cb2a100d71d0ed8a98a40f32cb75cf6cc08
Svetoslav Stoyanov <sgs.vss.svs.mvs.vvs@gmail.com>
2022-06-04 14:28:23 +0300

    Update ShowMore.js

--- DOM Introduction -  Lab/04. JS-Advanced-DOM-Introduction-Lab-Resources/04.Show More/ShowMore.js
+++ DOM Introduction -  Lab/04. JS-Advanced-DOM-Introduction-Lab-Resources/04.Show More/ShowMore.js
@@ -1,3 +1,6 @@
 function showText() {
-    // TODO
-}
+    let text = document.getElementById('text')
+    let more = document.getElementById('more')
+    more.style.display = 'none'
+    text.style.display = 'inline'
+}  
