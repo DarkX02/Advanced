d615632a7795fa32a26e4332ea160f4b8e672abe
Svetoslav Stoyanov <sgs.vss.svs.mvs.vvs@gmail.com>
2022-06-04 14:29:35 +0300

    Update ExtractParenthesis.js

--- DOM Introduction -  Lab/04. JS-Advanced-DOM-Introduction-Lab-Resources/07.Extract Parenthesis/ExtractParenthesis.js
+++ DOM Introduction -  Lab/04. JS-Advanced-DOM-Introduction-Lab-Resources/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,14 @@
 function extract(content) {
+    let pattern = /\([A-Za-z ]+\)/g
+    let text = document.getElementById(content).textContent
+    let match = text.match(pattern)
+    let result = []
+    for (let word of match) {
+        word = word.split('')
+        word.pop()
+        word.shift()
+        result.push(word.join(''))
 
+    }
+    return result.join('; ') + ';'
 }
