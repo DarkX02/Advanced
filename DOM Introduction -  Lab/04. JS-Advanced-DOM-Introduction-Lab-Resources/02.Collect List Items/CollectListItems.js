3daaaacf49055a182f79b1c28d414e519de7f769
Svetoslav Stoyanov <sgs.vss.svs.mvs.vvs@gmail.com>
2022-06-04 14:27:04 +0300

    Update CollectListItems.js

--- DOM Introduction -  Lab/04. JS-Advanced-DOM-Introduction-Lab-Resources/02.Collect List Items/CollectListItems.js
+++ DOM Introduction -  Lab/04. JS-Advanced-DOM-Introduction-Lab-Resources/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,10 @@
 function extractText() {
-    // TODO
+    let items = Array.from(document.getElementById('items').children)
+    let result = []
+    for (let el of items) {
+        result.push(el.textContent)
+    }
+    let textArea = document.getElementById('result')
+    textArea.value = result.join('\n')
+    console.log(result);
 }
